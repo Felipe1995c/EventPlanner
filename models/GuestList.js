9552eb9f7d04f6cc5988ab82b92ca40f8ed1b4bf
Irvin Lu <scurvyirv@scurvyirvy.attlocal.net>
2024-07-16 20:22:13 -0700

    feat: deleted location model and seed since it is front end, matched guestlist model and seeds, labeled user file

---
+++ models/GuestList.js
@@ -0,0 +1,44 @@
+// Import parts from sequelize library
+const { Model, DataTypes } = require('sequelize');
+
+// Import database connection from connection.js
+const sequelize = require('../config/database');
+
+// Initialize GuestList model by extending from Sequelize's Model class
+class GuestList extends Model {}
+
+// Establish fields and rules for GuestList model
+GuestList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'guestList',
+  }
+);
+
+module.exports = GuestList;
