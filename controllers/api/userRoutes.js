beccef22d9646de20e485ec832130f19d9a407d0
Alex Tran <Lockey5891@aol.com>
2024-07-18 11:53:52 -0700

    Edited code in homeRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -63,4 +63,37 @@ router.post("/logout", (req, res) => {
   }
 });
 
+// If a Post request is made to /api/users/signup,
+router.post("/signup", async (req, res) => {
+  console.log("Incoming Data: ", req.body)
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
