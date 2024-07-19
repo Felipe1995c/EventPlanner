5fba5a4f680a79061b504882a02485a9bd7f9ab8
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-07-18 21:30:53 -0700

    created index.js file to create associations

--- models/index.js
+++ models/index.js
@@ -1,1 +1,15 @@
 //place associations here
+const User = require('./user');
+const Budget = require('./budget');
+const GuestList = require('./GuestList');
+
+Budget.belongsTo(User, {
+    foreignKey: 'id'
+});
+
+
+GuestList.belongsTo(User, {
+    foreignKey: 'id'
+});
+
+module.exports = { User, Budget, GuestList}
