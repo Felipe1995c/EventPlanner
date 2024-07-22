1e61d99231c463bf84f23043a3d39282d268f83d
Irvin Lu <scurvyirv@scurvyirvy.attlocal.net>
2024-07-22 16:32:50 -0700

    feat: database seeding

--- seeds/budgetSeeds.js
+++ seeds/budgetSeeds.js
@@ -1,37 +1,33 @@
 //enforce strict mode for robust code
 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Budgets', [
-      {
-        category: 'Food',
-        amount: 500,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        category: 'Rentals',
-        amount: 300,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        category: 'Supplies',
-        amount: 150,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        category: 'Entertainment',
-        amount: 400,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ], {});
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Budgets', null, {});
-  }
+const seedBudgets = async (queryInterface, Sequelize) => {
+  await queryInterface.bulkInsert('Budgets', [
+    {
+      category: 'Food',
+      amount: 500,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      category: 'Rentals',
+      amount: 300,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      category: 'Supplies',
+      amount: 150,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      category: 'Entertainment',
+      amount: 400,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ], {});
 };
+
+module.exports = seedBudgets;
